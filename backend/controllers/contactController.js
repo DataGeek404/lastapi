825dc0caa4e303a2a58a53f17507eb435bd5399f
DataGeek404 <techjayprivate@gmail.com>
2025-05-20 01:00:24 +0300

    Update contactController.js

--- backend/controllers/contactController.js
+++ backend/controllers/contactController.js
@@ -9,6 +9,7 @@ const submitContactForm = async (req, res) => {
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
+  
 
   const { name, email, subject, message } = req.body;
   
