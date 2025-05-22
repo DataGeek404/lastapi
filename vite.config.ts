d149e85ba433972fcc722233eb431ce3b8b77b60
DataGeek404 <techjayprivate@gmailcom>
2025-05-22 12:23:25 +0300

    added procfile

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
-    allowedHosts: "all", // ✅ Allow all hosts
+    allowedHosts: "africanstreettherapymedicine-pxxyx.ondigitalocean.app", // ✅ Allow all hosts
   },
 
   plugins: [
